a05923f1e0a1d842517791a0b08c7705c21fe265
John-Feil <johnathancfeil@gmail.com>
2022-03-16 13:40:40 -0400

    Finised scanner hw

--- src/main/java/InClass.java
+++ src/main/java/InClass.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 public class InClass {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        int qty = 20;
-        float price = 5.0f;
+        String product = "";
+        System.out.println("What's the item called?");
+        product = userInput.nextLine();
+        int qty = 0;
+        System.out.println("What's the item's quantity");
+        qty = userInput.nextInt();
+        float price = 0.0f;
+        System.out.println("What's the item's price?");
+        price = userInput.nextFloat();
         float total = qty * price;
         String output = String.format("%s %8s %10s %10s","Product","QTY","Price","Total");
         String output1 = String.format("%s %11s %10s %10s","----","---","-----","-----");
-        String output2 = String.format("%s %12d %10.1f %10.1f","Mug",qty,price,total);
+        String output2 = String.format("%s %12d %10.1f %10.1f",product,qty,price,total);
         System.out.println(output);
         System.out.println(output1);
         System.out.println(output2);
