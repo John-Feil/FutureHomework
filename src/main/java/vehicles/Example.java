ecc7994b2362758b032d0422de6ae83b15382b4c
John-Feil <johnathancfeil@gmail.com>
2022-03-16 13:52:08 -0400

    Classwork

---
+++ src/main/java/vehicles/Example.java
@@ -0,0 +1,38 @@
+package vehicles;
+
+public class Example {
+    public static void main(String[] args) {
+        int days = 4;
+        switch (days) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            default:
+                System.out.println("Not a week day");
+        }
+        int age = 19;
+        if (age > 21){
+            System.out.println("Can drink");
+        }
+        else if (age == 21){
+            System.out.println("You can start drinking");
+        }
+        else {
+            System.out.println("Underage");
+        }
+        String canDrink = (age > 17) ? "Can party" : "Cannot party";
+        System.out.println(canDrink);
+    }
+}
